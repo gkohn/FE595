bc81f1f59e58382b9c6e32da939a736020748f66
gkohn <grace.kohn@gmail.com>
2019-02-04 22:15:09 -0600

    Add files via upload

---
+++ HW_1.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  4 21:25:09 2019
+
+@author: grace.kohn
+"""
+
+# Import Matplotlib and Numpy packages
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Generate Inputs (x)
+x = np.arange(0, 3*np.pi, 0.2);
+
+# Generate Wave Output (y)
+cos_wave = np.cos(x)
+sin_wave = np.sin(x)
+
+# Plot waves
+plt.plot(x, cos_wave)
+plt.plot(x, sin_wave)
+
+# Label Axes
+plt.xlabel('x')
+plt.ylabel('y')
+
+# Add grid to plot
+plt.grid(True, which='both')
+plt.axhline(y=0, color='b')
+
+ # Display plot
+plt.show()
